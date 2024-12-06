9648169f4524dc2dbe47e9f96c7414b7a599556a
Max <128371845+metrokot@users.noreply.github.com>
2024-12-06 08:49:26 +0300

    Update filesmanager.py

--- filesmanager.py
+++ filesmanager.py
@@ -1,3 +1,6 @@
+import hashlib
+import random
+
 class User():
     def __init__(self, params):
         print(params)
@@ -8,6 +11,7 @@ class User():
         self.withoutpass = params[4]
     def get(self):
         return f'{"|".join((self.login, self.password, self.mode, self.ban, self.withoutpass))}'+'\n'
+
 
 def checkUser(login, password):
     f = open("users.txt", 'r')
@@ -31,10 +35,12 @@ def checkUser(login, password):
 
 def setpassword(user, lastpass, newpass, confirmpass):
     error = 0
-    if lastpass != '':
+    if lastpass != user.password:
         error = 'Неверный предыдуший пароль'
     if newpass != confirmpass:
         error = 'Неверное подтверждение пароля'
+    if not setpasswordcheck(newpass):
+        error = 'Не соблюдены ограничения на пароль'
     if not error:
         with open('users.txt', 'r') as f:
             old_data = f.read()
@@ -43,62 +49,25 @@ def setpassword(user, lastpass, newpass, confirmpass):
             f.write(new_data)
     return error
 
-    
-def passChange(user):
-    lastpass = input("Введите старый пароль: ")
-    if lastpass != user.password:
-        print('Неверный предыдуший пароль')
-        if not contin():
-            return False
-        else:
-            return passChange(user)
-    newpass = input("Введите новый пароль: ")
-    with open('users.txt', 'r') as f:
-        old_data = f.read()
-    new_data = old_data.replace(f'{user.login}|{user.password}', f'{user.login}|{newpass}')
-    with open('users.txt', 'w') as f:
-        f.write(new_data)
-    return True
 
-def contin():
-    mode = int(input(
-'''Если хотите продолжить введите   1
-Если хотите выйти введите   0
-    '''))
-    if mode ==1:
-        return True
-    elif mode == 0:
-        return False
-    else:
-        print("Неверный ввод")
-        return contin()
 def newUser(login):
     f = open("users.txt", 'a')
     f.write(f'{login}||user|0|0' + '\n')
     return True
-def UserList(skip=0):
+
+
+def UserList():
     f = open("users.txt", 'r')
     f1 = open('users2.txt','w')
+    userList = []
     for line in f:
-        check = line.strip().split('|')
-        if not skip:
-            getUserParams(check)
-            edit = int(input('''Для изменения данных введите 1 
-Для показа следующего пользователя введите 2
-Для выхода нажмите 0'''))
-        else:
-            edit=2
-        if edit:
-            if edit==1:
-                check[0] = input("Введите новый логин")
-                check[1] = input("Введите новый пароль")
-                check[3] = input("Введите состояние блокировки")
-                check[4] = input("Введите состояние вход без пароля")
-            f1.write(f'{"|".join(check)}'+'\n')
-        else:
-            skip = 1
+        user = User(line.strip().split('|'))
+        userList.append(user)
     f.close()
     f1.close()
+    return userList
+
+
 def filechanger():
     f = open("users.txt", 'w')
     f1 = open('users2.txt', 'r')
@@ -106,11 +75,90 @@ def filechanger():
     f.write(content)
     f.close()
     f1.close()
-def getUserParams(list):
-    print(
-f'''
-Имя пользователя: {list[0]}
-Пароль: {list[1]}
-Блокировка: {list[3]}
-Вход без пароля: {list[4]}
-''')
+
+
+def predobrabot():
+    f = open('users.txt', 'r')
+    f2 = open("users2.txt", 'w')
+    for line in f:
+        if line!='\n' and line!='' and line!=None:
+            f2.write(line)
+    f.close()
+    f2.close()
+    filechanger()
+
+def setpasswordcheck(password):
+    tr=0
+    numb = list('1234567890')
+    zn = list(',.?!;')
+    zn2 = list('/*-+>=%^')
+    for elem in numb:
+        if elem in password:
+            tr+=1
+            break
+    for elem in zn:
+        if elem in password:
+            tr+=1
+            break
+    for elem in zn2:
+        if elem in password:
+            tr+=1
+            break
+    if tr==3:
+        return True
+    else:
+        return False
+
+
+def psevdoshifrSHA():
+    f = open('users.txt', 'r')
+    data = f.load()
+
+def shifrSHA():
+    hash = lambda data, salt: hashlib.sha256((salt + data).encode()).hexdigest()
+    f = open('users.txt', 'r')
+    blocks = [line for line in f]
+    f.close()
+    salt = str(random.randint(1, 1000000))
+    hashed_blocks = [hash(block, salt) for block in blocks]
+    combined_hash = hashed_blocks[0]
+    for block_hash in hashed_blocks[1:]:
+        combined_hash = hash(combined_hash, block_hash)
+    f = open('input.txt', 'w')
+    f.write(combined_hash)
+    '''
+    file_path = 'example.txt'
+    
+    with open(file_path, 'rb') as file:
+        while True:
+            data = file.read(1024)
+            if not data:
+                break
+            md5_hash.update(data)
+            sha256_hash.update(data)
+    
+    print(f'MD5: {md5_hash.hexdigest()}')
+    print(f'SHA-256: {sha256_hash.hexdigest()}')
+    '''
+def unshifrSHA():
+    pass
+def psevdoshifrMD5():
+    hash = lambda data, salt: hashlib.md5((salt + data).encode()).hexdigest()
+    f = open('users.txt', 'r')
+    blocks = [line for line in f]
+    f.close()
+    salt = str(random.randint(1, 1000000))
+    hashed_blocks = [hash(block, salt) for block in blocks]
+    combined_hash = hashed_blocks[0]
+    for block_hash in hashed_blocks[1:]:
+        combined_hash = hash(combined_hash, block_hash)
+    f = open('input.txt', 'w')
+    f.write(combined_hash)
+def unshifrMD5():
+    pass
+
+def keypass(key):
+    if key == 'pass':
+        return True
+    else:
+        False
